ba6edb76958794e25e2fcb37cc78e7e01d3c017d
guddnjs <guddnjs010@naver.com>
2022-11-30 13:11:24 +0900

    MVC_TEST
    MVC 모델 예제 테스트

---
+++ ex01/src/main/java/org/zerock/controller/HomeController.java
@@ -0,0 +1,42 @@
+package org.zerock.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+//servlet-context.xml에서 설정된 패키지의 @Controller를 읽음 
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	//화면에 전달하고 싶은 데이터는 Model model에 담아서 전송 model.addAtribute("key", Value);
+	//@RequestMapping() <- ()안의 들어오는 URI를 처리
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
